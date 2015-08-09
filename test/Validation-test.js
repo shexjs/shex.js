bec65b62ecbee2f19a9867ec2281a2c940db332e
Eric Prud'hommeaux <eric@w3.org>
2015-08-09 19:15:24 -0400

    + start on validation

---
+++ test/Validation-test.js
@@ -0,0 +1,91 @@
+//  "use strict";
+var VERBOSE = "VERBOSE" in process.env;
+var TESTS = "TESTS" in process.env ? process.env.TESTS.split(/,/) : null;
+
+var ShExParser = require("../lib/ShExParser").Parser;
+var ShExValidator = require("../lib/ShExValidator");
+
+var N3 = require("n3");
+var turtleParser = N3.Parser();
+var store = N3.Store();
+var fs = require("fs");
+var path = require("path");
+var expect = require("chai").expect;
+var findPath = require("./findPath.js");
+
+var schemasPath = findPath("schemas");
+var validationsPath = findPath("validations");
+var manifestFile = validationsPath+"manifest.json";
+var negSyntaxTestsPath = findPath("negativeSyntax");
+
+describe("A ShEx validator", function () {
+  // var b = function () {  };
+  // it("is a toy", function () {
+  //   expect({a:1, b: b}).to.deep.equal({a:1, b: b});
+  // });
+
+  var shexParser = new ShExParser();
+  var validator = new ShExValidator();
+
+  // Ensure the same blank node identifiers are used in every test
+  beforeEach(function () {
+    shexParser._resetBlanks();
+    validator.reset();
+  });
+
+  var tests = parseJSON(fs.readFileSync(manifestFile, "utf8")).tests;
+
+  tests.forEach(function (test) {
+
+    var schemaFile = path.join(schemasPath, test.schema);
+    var dataFile = path.join(validationsPath, test.data);
+    var resultsFile = path.join(validationsPath, test.result);
+    var schema = shexParser.parse(fs.readFileSync(schemaFile, "utf8"));
+
+    function done () {
+      var referenceResult = parseJSON(fs.readFileSync(resultsFile, "utf8"));
+
+      // var start = schema.start;
+      // if (start === undefined && Object.keys(schema.shapes).length === 1)
+      //   start = Object.keys(schema.shapes)[0];
+
+      var validator = ShExValidator(schema);
+
+      var validationResult = validator.validate(store, test.node, test.shape);
+      if (VERBOSE) console.log("result   :" + JSON.stringify(validationResult));
+      if (VERBOSE) console.log("expected :" + JSON.stringify(referenceResult));
+      expect(validationResult).to.deep.equal(referenceResult);
+    }
+
+    it("should validate data '" + (VERBOSE ? dataFile : test.schema) +
+       "' against schema '" + (VERBOSE ? schemaFile : test.data) +
+       "' and get '" + (VERBOSE ? resultsFile : test.result) + "'." , function (done) {
+	 
+	 turtleParser.parse(fs.readFileSync(dataFile, "utf8"),
+			    function (error, triple, prefixes) {
+			      if (triple)
+				store.addTriple(triple)
+			      else { debugger;
+				done();}
+			    });
+       });
+  });
+});
+
+// Parses a JSON object, restoring `undefined` values
+function parseJSON(string) {
+  var object = JSON.parse(string);
+  return /"\{undefined\}"/.test(string) ? restoreUndefined(object) : object;
+}
+
+// Recursively replace values of "{undefined}" by `undefined`
+function restoreUndefined(object) {
+  for (var key in object) {
+    var item = object[key];
+    if (typeof item === "object")
+      object[key] = restoreUndefined(item);
+    else if (item === "{undefined}")
+      object[key] = undefined;
+  }
+  return object;
+}
