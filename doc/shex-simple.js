98a891a051c94c711bf26e4a462fd2c3e4325f6d
Eric Prud'hommeaux <eric@w3.org>
2017-05-29 18:42:02 -0400

    ~ isolate IRIresolvers

--- doc/shex-simple.js
+++ doc/shex-simple.js
@@ -19,6 +19,9 @@ function parseTurtle (text, meta) {
     ret.addTriples(triples);
   meta.base = parser._base;
   meta.prefixes = parser._prefixes;
+  var resolver = new IRIResolver(meta);
+  meta.termToLex = function (lex) { return  rdflib_termToLex(lex, resolver); };
+  meta.lexToTerm = function (lex) { return  rdflib_lexToTerm(lex, resolver); };
   return ret;
 }
 
@@ -28,6 +31,9 @@ function parseShEx (text, meta) {
   var ret = shexParser.parse(text);
   meta.base = ret.base;
   meta.prefixes = ret.prefixes;
+  var resolver = new IRIResolver(meta);
+  meta.termToLex = function (lex) { return  rdflib_termToLex(lex, resolver); };
+  meta.lexToTerm = function (lex) { return  rdflib_lexToTerm(lex, resolver); };
   return ret;
 }
 
@@ -39,10 +45,10 @@ function sum (s) { // cheap way to identify identical strings
 }
 
 // <n3.js-specific>
-function termToLex (node, prefixes) {
-  return node === "- start -" ? node : N3Writer({ prefixes:prefixes || {} })._encodeObject(node);
+function rdflib_termToLex (node, resolver) {
+  return node === "- start -" ? node : N3Writer({ prefixes:resolver.meta.prefixes || {} })._encodeObject(node);
 }
-function lexToTerm (lex, resolver) {
+function rdflib_lexToTerm (lex, resolver) {
   return lex === "- start -" ? lex : N3Lexer().tokenize(lex).map(token => {
     var left = 
           token.type === "typeIRI" ? "^^" :
@@ -134,7 +140,7 @@ function makeSchemaCache (parseSelector) {
   ret.getShapes = function () {
     var obj = this.refresh();
     var start = "start" in obj ? [START_SHAPE_LABEL] : [];
-    var rest = "shapes" in obj ? Object.keys(obj.shapes).map(termToLex) : [];
+    var rest = "shapes" in obj ? Object.keys(obj.shapes).map(InputSchema.meta.termToLex) : [];
     return start.concat(rest);
   };
   return ret;
@@ -149,11 +155,132 @@ function makeTurtleCache(parseSelector) {
   ret.getNodes = function () {
     var data = this.refresh();
     return data.getTriples().map(t => {
-      return termToLex(t.subject);
+      return InputData.meta.termToLex(t.subject);
     });
   };
   return ret;
 }
+
+function IRIResolver (meta) {
+  if (!(this instanceof IRIResolver))
+    return new IRIResolver(options);
+  this.meta = meta;
+  this._setBase(meta.base);
+};
+
+var absoluteIRI = /^[a-z][a-z0-9+.-]*:/i,
+    schemeAuthority = /^(?:([a-z][a-z0-9+.-]*:))?(?:\/\/[^\/]*)?/i,
+    dotSegments = /(?:^|\/)\.\.?(?:$|[\/#?])/;
+IRIResolver.prototype = {
+
+  // vvv stolen from Ruben Vergorgh's N3Parser.js vvv
+  // ### `_setBase` sets the base IRI to resolve relative IRIs
+  _setBase: function (baseIRI) {
+    if (!baseIRI)
+      this._base = null;
+    else {
+      // Remove fragment if present
+      var fragmentPos = baseIRI.indexOf('#');
+      if (fragmentPos >= 0)
+        baseIRI = baseIRI.substr(0, fragmentPos);
+      // Set base IRI and its components
+      this._base = baseIRI;
+      this._basePath   = baseIRI.indexOf('/') < 0 ? baseIRI :
+        baseIRI.replace(/[^\/?]*(?:\?.*)?$/, '');
+      baseIRI = baseIRI.match(schemeAuthority);
+      this._baseRoot   = baseIRI[0];
+      this._baseScheme = baseIRI[1];
+    }
+  },
+
+  // ### `_resolveIRI` resolves a relative IRI token against the base path,
+  // assuming that a base path has been set and that the IRI is indeed relative
+  _resolveIRI: function (token) {
+    var iri = token.value;
+    switch (iri[0]) {
+      // An empty relative IRI indicates the base IRI
+    case undefined: return this._base;
+      // Resolve relative fragment IRIs against the base IRI
+    case '#': return this._base + iri;
+      // Resolve relative query string IRIs by replacing the query string
+    case '?': return this._base.replace(/(?:\?.*)?$/, iri);
+      // Resolve root-relative IRIs at the root of the base IRI
+    case '/':
+      // Resolve scheme-relative IRIs to the scheme
+      return (iri[1] === '/' ? this._baseScheme : this._baseRoot) + this._removeDotSegments(iri);
+      // Resolve all other IRIs at the base IRI's path
+    default:
+      return this._removeDotSegments(this._basePath + iri);
+    }
+  },
+
+  // ### `_removeDotSegments` resolves './' and '../' path segments in an IRI as per RFC3986
+  _removeDotSegments: function (iri) {
+    // Don't modify the IRI if it does not contain any dot segments
+    if (!dotSegments.test(iri))
+      return iri;
+
+    // Start with an imaginary slash before the IRI in order to resolve trailing './' and '../'
+    var result = '', length = iri.length, i = -1, pathStart = -1, segmentStart = 0, next = '/';
+
+    while (i < length) {
+      switch (next) {
+        // The path starts with the first slash after the authority
+      case ':':
+        if (pathStart < 0) {
+          // Skip two slashes before the authority
+          if (iri[++i] === '/' && iri[++i] === '/')
+            // Skip to slash after the authority
+            while ((pathStart = i + 1) < length && iri[pathStart] !== '/')
+              i = pathStart;
+        }
+        break;
+        // Don't modify a query string or fragment
+      case '?':
+      case '#':
+        i = length;
+        break;
+        // Handle '/.' or '/..' path segments
+      case '/':
+        if (iri[i + 1] === '.') {
+          next = iri[++i + 1];
+          switch (next) {
+            // Remove a '/.' segment
+          case '/':
+            result += iri.substring(segmentStart, i - 1);
+            segmentStart = i + 1;
+            break;
+            // Remove a trailing '/.' segment
+          case undefined:
+          case '?':
+          case '#':
+            return result + iri.substring(segmentStart, i) + iri.substr(i + 1);
+            // Remove a '/..' segment
+          case '.':
+            next = iri[++i + 1];
+            if (next === undefined || next === '/' || next === '?' || next === '#') {
+              result += iri.substring(segmentStart, i - 2);
+              // Try to remove the parent path from result
+              if ((segmentStart = result.lastIndexOf('/')) >= pathStart)
+                result = result.substr(0, segmentStart);
+              // Remove a trailing '/..' segment
+              if (next !== '/')
+                return result + '/' + iri.substr(i + 1);
+              segmentStart = i + 1;
+            }
+          }
+        }
+      }
+      next = iri[++i];
+    }
+    return result + iri.substring(segmentStart);
+  },
+
+  _resolveAbsoluteIRI: function  (token) {
+    return (this._base === null || absoluteIRI.test(token.value)) ?
+      token.value : this._resolveIRI(token);
+  }
+};
 
 
 // controls for example links
@@ -272,7 +399,6 @@ function disableResultsAndValidate () {
   results.start();
   setTimeout(function () {
     validate();
-    results.finish();
   }, 0);
 }
 
@@ -291,6 +417,7 @@ function validate () {
   $("#results .status").hide();
   var parsing = "input schema";
   try {
+    InputSchema.refresh();
     $("#schemaDialect").text(InputSchema.language);
     InputData.refresh(); // for prefixes for getShapeMap
     var dataText = InputData.get();
@@ -367,7 +494,7 @@ function validate () {
     switch (interface) {
     case "human":
       var elt = $("<div class='human'/>").text(
-        `${termToLex(entry.node)}@${fails ? "!" : ""}${termToLex(entry.shape)}`
+        `${InputSchema.meta.termToLex(entry.node)}@${fails ? "!" : ""}${InputData.meta.termToLex(entry.shape)}`
       ).addClass(klass);
       if (fails)
         elt.append($("<pre>").text(ShExUtil.errsToSimple(entry.appinfo).join("\n")));
@@ -492,12 +619,11 @@ var parseQueryString = function(query) {
  * use {InputData,InputSchema}.meta.{prefix,base} to complete IRIs
  */
 function parseUIShapeMap () {
-  var [dataResolver, schemaResolver] = [new IRIResolver(InputData.meta), new IRIResolver(InputSchema.meta)];
   var mapAndErrors = $(".pair").get().reduce((acc, pair) => {
     var node = $(pair).find(".focus").val();
     var shape = $(pair).find(".inputShape").val();
-    $(pair).attr("data-node", lexToTerm(node, dataResolver));
-    $(pair).attr("data-shape", lexToTerm(shape, schemaResolver));
+    $(pair).attr("data-node", InputData.meta.lexToTerm(node));
+    $(pair).attr("data-shape", InputSchema.meta.lexToTerm(shape));
     if (node && shape)
       acc.shapeMap.push({node: node, shape: shape});
     return acc;
@@ -541,135 +667,13 @@ function parseUIShapeMap () {
   return mapAndErrors.shapeMap;
 }
 
-function IRIResolver (meta) {
-  if (!(this instanceof IRIResolver))
-    return new IRIResolver(options);
-  this.meta = meta;
-  this._setBase(meta.base);
-}
-
-var absoluteIRI = /^[a-z][a-z0-9+.-]*:/i,
-    schemeAuthority = /^(?:([a-z][a-z0-9+.-]*:))?(?:\/\/[^\/]*)?/i,
-    dotSegments = /(?:^|\/)\.\.?(?:$|[\/#?])/;
-IRIResolver.prototype = {
-
-  // vvv stolen from Ruben Vergorgh's N3Parser.js vvv
-  // ### `_setBase` sets the base IRI to resolve relative IRIs
-  _setBase: function (baseIRI) {
-    if (!baseIRI)
-      this._base = null;
-    else {
-      // Remove fragment if present
-      var fragmentPos = baseIRI.indexOf('#');
-      if (fragmentPos >= 0)
-        baseIRI = baseIRI.substr(0, fragmentPos);
-      // Set base IRI and its components
-      this._base = baseIRI;
-      this._basePath   = baseIRI.indexOf('/') < 0 ? baseIRI :
-        baseIRI.replace(/[^\/?]*(?:\?.*)?$/, '');
-      baseIRI = baseIRI.match(schemeAuthority);
-      this._baseRoot   = baseIRI[0];
-      this._baseScheme = baseIRI[1];
-    }
-  },
-
-  // ### `_resolveIRI` resolves a relative IRI token against the base path,
-  // assuming that a base path has been set and that the IRI is indeed relative
-  _resolveIRI: function (token) {
-    var iri = token.value;
-    switch (iri[0]) {
-      // An empty relative IRI indicates the base IRI
-    case undefined: return this._base;
-      // Resolve relative fragment IRIs against the base IRI
-    case '#': return this._base + iri;
-      // Resolve relative query string IRIs by replacing the query string
-    case '?': return this._base.replace(/(?:\?.*)?$/, iri);
-      // Resolve root-relative IRIs at the root of the base IRI
-    case '/':
-      // Resolve scheme-relative IRIs to the scheme
-      return (iri[1] === '/' ? this._baseScheme : this._baseRoot) + this._removeDotSegments(iri);
-      // Resolve all other IRIs at the base IRI's path
-    default:
-      return this._removeDotSegments(this._basePath + iri);
-    }
-  },
-
-  // ### `_removeDotSegments` resolves './' and '../' path segments in an IRI as per RFC3986
-  _removeDotSegments: function (iri) {
-    // Don't modify the IRI if it does not contain any dot segments
-    if (!dotSegments.test(iri))
-      return iri;
-
-    // Start with an imaginary slash before the IRI in order to resolve trailing './' and '../'
-    var result = '', length = iri.length, i = -1, pathStart = -1, segmentStart = 0, next = '/';
-
-    while (i < length) {
-      switch (next) {
-        // The path starts with the first slash after the authority
-      case ':':
-        if (pathStart < 0) {
-          // Skip two slashes before the authority
-          if (iri[++i] === '/' && iri[++i] === '/')
-            // Skip to slash after the authority
-            while ((pathStart = i + 1) < length && iri[pathStart] !== '/')
-              i = pathStart;
-        }
-        break;
-        // Don't modify a query string or fragment
-      case '?':
-      case '#':
-        i = length;
-        break;
-        // Handle '/.' or '/..' path segments
-      case '/':
-        if (iri[i + 1] === '.') {
-          next = iri[++i + 1];
-          switch (next) {
-            // Remove a '/.' segment
-          case '/':
-            result += iri.substring(segmentStart, i - 1);
-            segmentStart = i + 1;
-            break;
-            // Remove a trailing '/.' segment
-          case undefined:
-          case '?':
-          case '#':
-            return result + iri.substring(segmentStart, i) + iri.substr(i + 1);
-            // Remove a '/..' segment
-          case '.':
-            next = iri[++i + 1];
-            if (next === undefined || next === '/' || next === '?' || next === '#') {
-              result += iri.substring(segmentStart, i - 2);
-              // Try to remove the parent path from result
-              if ((segmentStart = result.lastIndexOf('/')) >= pathStart)
-                result = result.substr(0, segmentStart);
-              // Remove a trailing '/..' segment
-              if (next !== '/')
-                return result + '/' + iri.substr(i + 1);
-              segmentStart = i + 1;
-            }
-          }
-        }
-      }
-      next = iri[++i];
-    }
-    return result + iri.substring(segmentStart);
-  },
-
-  _resolveAbsoluteIRI: function  (token) {
-    return (this._base === null || absoluteIRI.test(token.value)) ?
-      token.value : this._resolveIRI(token);
-  }
-
-};
-
 /** shapeMapToTerms -- map ShapeMap to API terms
  * @@TODO: add to ShExValidator so API accepts ShapeMap
  */
 function shapeMapToTerms (shapeMap) {
-  var [dataResolver, schemaResolver] = [new IRIResolver(InputData.meta), new IRIResolver(InputSchema.meta)];
   return shapeMap.map(pair => {
-    return {node: lexToTerm(pair.node, dataResolver), shape: lexToTerm(pair.shape, schemaResolver)};
+    return {node: InputData.meta.lexToTerm(pair.node),
+            shape: InputSchema.meta.lexToTerm(pair.shape)};
   });
 }
 
