9d260839112ae95500efd04c64cda890d58577e4
Eric Prud'hommeaux <eric@w3.org>
2017-06-27 13:00:08 -0400

    + controls for metadata rendering

--- doc/shex-simple.js
+++ doc/shex-simple.js
@@ -216,7 +216,10 @@ function pickSchema (name, schemaTest, elt, listItems, side) {
     InputSchema.set(schemaTest.schema);
     $("#inputSchema .status").text(name);
 
-    InputMeta.set(schemaTest.meta);
+    var hasMeta = "meta" in schemaTest && schemaTest.meta.length > 0;
+    InputMeta.set(hasMeta ? schemaTest.meta : "");
+    $("#showMeta").prop("checked", hasMeta);
+    showMeta();
 
     InputData.set("");
     $("#inputData .status").text(" ");
@@ -515,6 +518,7 @@ function prepareControls () {
   $("#interface").on("change", setInterface);
   $("#validate").on("click", disableResultsAndValidate);
   $("#clear").on("click", clearAll);
+  $("#showMeta").on("click", showMeta);
 
   $("#loadForm").dialog({
     autoOpen: false,
@@ -675,6 +679,18 @@ function toggleControlsArrow (which) {
     break;
   default:
     throw Error("toggleControlsArrow expected [up|down], got \"" + which + "\"");
+  }
+}
+
+function showMeta () {
+  if ($("#showMeta").is(":checked")) {
+    $("#meta").show();
+    if ($("#meta").attr("data-adjust"))
+      $($("#meta").attr("data-adjust")).attr("rows", "12");
+  } else {
+    $("#meta").hide();
+    if ($("#meta").attr("data-adjust"))
+      $($("#meta").attr("data-adjust")).attr("rows", "25");
   }
 }
 
