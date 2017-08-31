9c08c4756208d9ed6abcad66993bce75d2953432
Eric Prud'hommeaux <eric@w3.org>
2017-08-31 13:06:48 -0400

    - redundant decl of schemas

--- test/Parser-Writer-test.js
+++ test/Parser-Writer-test.js
@@ -38,11 +38,6 @@ var schemas = parseJSONFile(manifestFile)["@graph"][0]["entries"];
 if (TESTS)
   schemas = schemas.filter(function (t) { return TESTS.indexOf(t.name) !== -1; });
 
-// positive transformation tests
-var schemas = parseJSONFile(manifestFile)["@graph"][0]["entries"];
-if (TESTS)
-  schemas = schemas.filter(function (t) { return TESTS.indexOf(t.name) !== -1; });
-
 describe("A ShEx parser", function () {
   // var b = function () {  };
   // it("is a toy", function () {
@@ -173,8 +168,8 @@ describe("A ShEx parser", function () {
   });
 
 
+  // negative syntax and structure tests
   negativeTests.forEach(testSet => {
-    // negative syntax tests
     var negSyntaxTests = fs.readdirSync(testSet.path).
         filter(function (s) { return s.indexOf(".shex") !== -1; });
     if (TESTS)
