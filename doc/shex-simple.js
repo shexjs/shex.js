81cb5ecdfb6ac53715ec3ea9154df9863311a896
Eric Prud'hommeaux <eric@w3.org>
2017-11-24 00:49:06 -0500

    ~ cache name typo

--- doc/shex-simple.js
+++ doc/shex-simple.js
@@ -24,7 +24,7 @@ var QueryParams = [
   {queryStringParm: "schema",       location: Caches.inputSchema.selection, cache: Caches.inputSchema },
   {queryStringParm: "data",         location: Caches.inputData.selection,   cache: Caches.inputData   },
   {queryStringParm: "shape-map",    location: $("#textMap"),    cache: Caches.shapeMap    },
-  {queryStringParm: "meta",         location: Caches.inputMeta.selection,   cache: Cache.inputMeta},
+  {queryStringParm: "meta",         location: Caches.inputMeta.selection,   cache: Caches.inputMeta},
   {queryStringParm: "interface",    location: $("#interface"),       deflt: "human"     },
   {queryStringParm: "regexpEngine", location: $("#regexpEngine"),    deflt: "threaded-val-nerr" },
 ];
