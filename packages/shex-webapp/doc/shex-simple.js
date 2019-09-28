ce9ed2cd3a8406a6741c926dd9b5b364ee8dd058
Eric Prud'hommeaux <eric+github@w3.org>
2019-09-28 02:06:53 +0200

    Revert "Hide “Manifest:” text if there are no manifests"
    This reverts commit a2c22b80e6070d1bf6ad345a945731eaaf18c982.

--- packages/shex-webapp/doc/shex-simple.js
+++ packages/shex-webapp/doc/shex-simple.js
@@ -1932,7 +1932,6 @@ function prepareManifest (demoList, base) {
   }, {});
   var nestingAsList = Object.keys(nesting).map(e => nesting[e]);
   paintManifest("#inputSchema .manifest ul", nestingAsList, pickSchema, listItems, "inputSchema");
-  $("#inputSchema .manifest").css("visibility", nestingAsList.length > 0 ? "visible" : "hidden");
   var timeouts = Object.keys(Caches).reduce((acc, k) => {
     acc[k] = undefined;
     return acc;
