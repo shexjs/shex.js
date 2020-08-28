25c59b76fc0264ba8913e63e2e065a332dcc402c
Eric Prud'hommeaux <eric+github@w3.org>
2020-08-28 15:02:41 +0200

    ~ move webpack to shex-webapp

---
+++ packages/shex-webapp/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    "shex-webapp-webpack": "./shex-webapp.js",
+    "shex-webapp-webpack.min": "./shex-webapp.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, 'browser'),
+    // libraryTarget: 'umd',
+    // libraryExport: 'ShExWebApp',
+    // umdNamedDefine: true,
+    // // globalObject: 'this'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        "keep_classnames": false
+      },
+      include: /\.min\.js$/
+    })]
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
+};
