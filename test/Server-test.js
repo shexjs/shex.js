7f50ff124df239c92dcfda72dc766ec2fb265b2c
Eric Prud'hommeaux <eric+github@w3.org>
2020-10-07 12:09:42 +0200

    ~ restructure Server-test.js

--- test/Server-test.js
+++ test/Server-test.js
@@ -34,20 +34,26 @@ const RestTests = {
     posts: [
       { curl: ["-F", "data=@./test/cli/p2p3.ttl", "-F", "node=x"], result: "cli/1dotOr2dot_pass_p1.val", status: 400 }
     ]
-  },
-  "default-schema": {
+  },*/
+/*  "default-schema": {
     args: ["-x", "cli/1dotOr2dot.shex", "-s", "<http://a.example/S1>"],
     posts: [
       { curl: ["-F", "data=@./test/cli/p2p3.ttl", "-F", "node=x"], result: "cli/1dotOr2dot_pass_p1.val", status: 200 }
     ]
-  },
-  "default-data": {
+  },*/
+/*  "default-data": {
     args: ["-d", "cli/p1.ttl", "-n", "<x>"],
     posts: [
       { curl: ["-F", "data=@./test/cli/p2p3.ttl", "-F", "node=x"], result: "cli/1dotOr2dot_pass_p1.val", status: 400 }
     ]
   },*/
   "default-schema-and-data": {
+    args: ["-x", "cli/1dotOr2dot.shex", "-s", "<http://a.example/S1>", "-d", "cli/p1.ttl", "-n", "<x>"],
+    posts: [
+      { curl: ["-F", "data=@./test/cli/p2p3.ttl", "-F", "node=x"], result: "cli/1dotOr2dot_pass_p1.val", status: 200 }
+    ]
+  },
+  "default-schema-and-dataB": {
     args: ["-x", "cli/1dotOr2dot.shex", "-s", "<http://a.example/S1>", "-d", "cli/p1.ttl", "-n", "<x>"],
     posts: [
       { curl: ["-F", "data=@./test/cli/p2p3.ttl", "-F", "node=x"], result: "cli/1dotOr2dot_pass_p1.val", status: 200 }
@@ -60,95 +66,109 @@ if (!TEST_server) {
 
 } else {
 
-  const serveArgs = ["-S", "http://localhost:8088/validate", "--serve-n", "1", "-Q"];
-  const serverUrl = "http://localhost:8088/validate";
+  const ServeArgs = ["-S", "http://localhost:8088/validate", "--serve-n", "1", "-Q"];
+  const ServerEndpoint = "http://localhost:8088/validate";
 
-  before(async () => {
-    Object.keys(RestTests).map(async function (serverName) {
-      let serverTest = RestTests[serverName];
+  async function tryPostToServer (serverName, serverTest, postTest) {
+    let server = null;
+    try {
+      server = await startServer(serverTest.args);
+    } catch (e) {
+      throw makeError(`Error in ${serverName} starting server with ${serverTest.args.join(' ')}`, e);
+    }
+    try {
+      return await executePost(server, postTest);
+    } catch (e) {
+      server.kill();
+      throw makeError(`Error calling server ${serverName} with ${postTest.curl}`, e);
+    }
+  }
 
-      if (TESTS)
-        serverTest = serverTest.filter(function (t) {
-          return TESTS.indexOf(t.name) !== -1;
-        });
+  function makeError (msg, caughtError) {
+    const throwMe = new Error(msg + ": " + caughtError);
+    if (caughtError.stack)
+      throwMe.stack = msg + ": " + caughtError.stack;
+    return throwMe;
+  }
 
-      await Promise.all(serverTest.posts.map(async function (postTest) {
-        try {
-          postTest.ref = ShExNode.GET(fromHere(postTest.result)).then(function (loaded) { return { result: loaded }; });
-          postTest.exec = postToServer(serverTest.args, postTest);
-          await Promise.all([postTest.ref, postTest.exec]);
-        } catch (e) {
-          const msg = `Error setting up test ${serverName} ${postTest.curl}`
-          const throwMe = new Error(msg + " " + e);
-          throwMe.stack = msg + " " + e.stack;
-          throw throwMe;
-        }
-      }))
-    })
+  async function startServer (serverArgs) {
+    const allArgs = ServeArgs.concat(serverArgs);
+    const server = child_process.spawn("../node_modules/.bin/shex-validate", allArgs, {cwd: __dirname});
+    const [host, port] = await new Promise((resolve, reject) => {
+      server.stdout.on("data", expectHostPort);
+      server.stderr.on("data", expectNothing);
 
-    async function postToServer (serverArgs, postTest) {
-      const server = child_process.spawn("../node_modules/.bin/shex-validate", serveArgs.concat(serverArgs), {cwd: __dirname});
-      const [host, port] = await new Promise((resolve, reject) => {
-        server.stdout.on("data", function (data) {
-          const text = data.toString('utf8');
-          const m = text.match(/^host:\s*([^ ]+)\s+port:\s*([0-9]+)\n$/);
-          if (m) {
-            resolve([m[1], m[2]]);
-          } else {
-            reject(Error(`Server start failure, saw "${text}"`));
-          }
-        });
-        server.stderr.on("data", function (data) { reject(data); });
-      })
-
-      const parms = [];
-      for (let i = 0; i < postTest.curl.length; ++i) {
-        const arg = postTest.curl[i];
-        switch (arg) {
-        case '-F':
-          const next = postTest.curl[++i];
-          const equals = next.indexOf('=');
-          let [attr, value] = [next.substr(0, equals), next.substr(equals + 1)];
-          if (value.startsWith('@'))
-            value = Fs.readFileSync(value.substr(1), "utf8");
-          parms.push({attr, value});
-          break;
-        default:
-          throw Error(`unhandled curl argument at: ${postTest.curl.slice(i)}`)
+      function expectHostPort (data) {
+        const text = data.toString('utf8');
+        const m = text.match(/^host:\s*([^ ]+)\s+port:\s*([0-9]+)\n$/);
+        if (m) {
+          server.stdout.removeListener("data", expectHostPort);
+          server.stderr.removeListener("data", expectNothing);
+          resolve([m[1], m[2]]);
+        } else {
+          server.kill();
+          reject(Error(`Server start failure, saw "${text}"`));
         }
       }
 
-      const postBody = parms.map(
-        pair =>
-          ['attr', 'value'].map(s => encodeURIComponent(s)).join("=")
-      ).join("\n");
-      const resp = await Fetch(serverUrl, {
-        method: "POST",
-        headers: {},
-        body: postBody
-      });
-      const respBody = await resp.text();
-      if (!resp.ok)
-        throw Error(`POST ${serverUrl} ${postBody}\n${respBody}`);
-      return await new Promise((resolve, reject) => {
-        let stdout = "", stderr = ""
+      function expectNothing (data) {
+        server.kill();
+        reject(Error(`unexpected stderr starting server: ${data.toString('utf8')}`));
+      }
+    })
+    return server;
+  }
 
-        server.stdout.on("data", function (data) { stdout += data; });
-        server.stderr.on("data", function (data) { stderr += data; });
-        server.on("close", function (exitCode) {
-          resolve({stdout:stdout, stderr:stderr, exitCode:exitCode, respBody, status: resp.status})
-        });
-        server.on("error", function(err) { reject(err); });
+  async function executePost (server, postTest) {
+    const parms = [];
+    for (let i = 0; i < postTest.curl.length; ++i) {
+      const arg = postTest.curl[i];
+      switch (arg) {
+      case '-F':
+        const next = postTest.curl[++i];
+        const equals = next.indexOf('=');
+        let [attr, value] = [next.substr(0, equals), next.substr(equals + 1)];
+        if (value.startsWith('@'))
+          value = Fs.readFileSync(value.substr(1), "utf8");
+        parms.push({attr, value});
+        break;
+      default:
+        throw Error(`unhandled curl argument at: ${postTest.curl.slice(i).join(' ')}`)
+      }
+    }
+    const postBody = parms.map(
+      pair =>
+        ['attr', 'value'].map(s => encodeURIComponent(s)).join("=")
+    ).join("\n");
+
+    const serverOutput = new Promise((resolve, reject) => {
+      let stdout = "", stderr = ""
+
+      server.stdout.on("data", function (data) { stdout += data; });
+      server.stderr.on("data", function (data) { stderr += data; });
+      server.on("close", function (exitCode) {
+        resolve({stdout:stdout, stderr:stderr, exitCode:exitCode, respBody, status: resp.status})
       });
-    };
-  });
+      server.on("error", function(err) { reject(err); });
+    });
+    const resp = await Fetch(ServerEndpoint, {
+      method: "POST",
+      headers: {},
+      body: postBody
+    });
+    const respBody = await resp.text();
+
+    if (!resp.ok)
+      throw Error(`POST ${ServerEndpoint} ${postBody}\n${respBody}`);
+    return await serverOutput;
+  };
 
   /* test results
    */
   Object.keys(RestTests).forEach(function (serverName) {
-    let serverTest = RestTests[serverName].posts;
+    let serverTest = RestTests[serverName];
 
-    describe("The " + serverName + " serverName", function () {
+    describe(`The ${serverName} server`, function () {
       "use strict";
 
       const setSlow = process.env["CLI_TIMEOUT"]; // CLI_TIMEOUT=4000 will run tests with timout of 4s
@@ -157,43 +177,44 @@ if (!TEST_server) {
         serverTest = serverTest.filter(function (t) {
           return TESTS.indexOf(t.name) !== -1;
         });
-
-      serverTest.forEach(function (postTest) {
+      serverTest.posts.forEach(function (postTest) {
         it("should execute $(" + postTest.curl.join(" ") + ")"+
            ( "resultMatch" in postTest ?
              (" and match /" + postTest.resultMatch) + "/" :
-             (" and get " +
+             (" and curl " +
               ("resultText" in postTest ? JSON.stringify(postTest.resultText) :
                "resultNoSpace" in postTest ? JSON.stringify(postTest.resultNoSpace) : postTest.result))
            ) +
            " in postTest '" + postTest.curl.join(' ') + "'.",
-           function (done) {
+           async function () {
              // stamp(serverName+"/"+postTest.name);
-             Promise.all([postTest.ref, postTest.exec]).then(function (both) {
-               const [ref, exec] = both;
+             const ref = {
+               result: await ShExNode.GET(fromHere(postTest.result))
+             }
+             const exec = await tryPostToServer(serverName, serverTest, postTest);
 
-               if (postTest.status === 0) {      // Keep this test before exitCode in order to
-                 expect(exec.stderr).to.be.empty; // print errors from spawn.
-               }
+             if (postTest.status === 0) {      // Keep this test before exitCode in order to
+               expect(exec.stderr).to.be.empty; // print errors from spawn.
+             }
 
-               if ("errorMatch" in ref)
-                 expect(exec.stderr).to.match(ref.errorMatch);
-               if ("resultMatch" in ref)
-                 expect(exec.stdout).to.match(ref.resultMatch);
-               else if ("resultText" in ref)
-                 expect(exec.stdout).to.equal(ref.resultText);
-               else if ("resultNoSpace" in ref)
-                 expect(exec.stdout.replace(/[ \n]/g, "")).to.equal(ref.resultNoSpace.text.replace(/[ \n]/g, ""));
-               else if ("result" in ref) {
-                 expect(JSON.parse(exec.respBody)).to.deep.equal(
-                   ShExUtil.absolutizeResults(
-                     JSON.parse(ref.result.text), ref.result.url));}
-               else if (!("errorMatch" in ref))
-                 throw Error("unknown test criteria in " + JSON.stringify(ref));
-
-               expect(exec.status).to.equal(postTest.status);
-               done();
-             }).catch(function (e) { done(e); });
+             if ("resultMatch" in ref)
+               expect(exec.stdout).to.match(ref.resultMatch);
+             else if ("resultText" in ref)
+               expect(exec.stdout).to.equal(ref.resultText);
+             else if ("resultNoSpace" in ref)
+               expect(exec.stdout.replace(/[ \n]/g, "")).to.equal(ref.resultNoSpace.text.replace(/[ \n]/g, ""));
+             else if ("result" in ref) {
+               expect(JSON.parse(exec.respBody)).to.deep.equal(
+                 ShExUtil.absolutizeResults(
+                   JSON.parse(ref.result.text), ref.result.url));}
+             else if (!("errorMatch" in ref))
+               throw Error("unknown test criteria in " + JSON.stringify(ref));
+             const m = exec.stdout.match(/^([a-f0-9:.]+): (GET|POST) ([^ ]+) ([0-9]+) ([a-z]+)\/([a-z]+) ([0-9]+)\n$/);
+             expect(m).to.not.be.null;
+             const [undefined, myIp, method, path, status, tree, subtype, size] = m;
+             expect(parseInt(status)).to.equal(postTest.status);
+             // console.warn([myIp, method, path, status, tree, subtype, size])
+             expect(exec.status).to.equal(postTest.status);
            }).timeout(2 * 60 * 1000); // 2 mins for the whole test suite
       });
     });
