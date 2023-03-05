5352457e037fa93a70e210dcca18f9f151c31cf0
Eric Prud'hommeaux <eric+github@w3.org>
2023-03-06 00:05:04 +0100

    + ShExMapBaseApp, prepareManifest, queryMapLoaded

--- packages/extension-map/doc/ShExMapBaseApp.js
+++ packages/extension-map/doc/ShExMapBaseApp.js
@@ -15,19 +15,49 @@ class JSONCache extends InterfaceCache {
   }
 }
 
-class ShExMapBaseApp extends ShExSimpleApp {
+class ShExMapManifestCache extends ManifestCache {
+  makeDataEntry (dataLabel, idx, elt, base) {
+    const ret = super.makeDataEntry(dataLabel, idx, elt, base);
+    ret.outputSchemaUrl = elt.outputSchemaURL || (elt.outputSchema ? base : undefined)
+    return ret;
+  }
+
+  async queryMapLoaded (dataTest, text) {
+    await super.queryMapLoaded(dataTest, text);
+    /* This is kind of a wart 'cause I haven't made a 3rd level of manifest entry for materialization */
+    if (dataTest.entry.outputSchema === undefined && dataTest.entry.outputSchemaURL) {
+      dataTest.entry.outputSchemaURL = new URL(dataTest.entry.outputSchemaURL, dataTest.url).href; // absolutize
+      const resp = await fetch(dataTest.entry.outputSchemaURL);
+      if (!resp.ok)
+        throw Error("fetch <" + dataTest.entry.outputSchemaURL + "> got error response " + resp.status + ": " + resp.statusText);
+      dataTest.entry.outputSchema = await resp.text();
+    }
+    App.Caches.outputSchema.set(dataTest.entry.outputSchema, dataTest.entry.outputSchemaURL);
+    $("#outputSchema .status").text(name);
+    App.Caches.statics.set(JSON.stringify(dataTest.entry.staticVars, null, "  "));
+    $("#staticVars .status").text(name);
+
+    $("#outputShape").val(dataTest.entry.outputShape); // targetSchema.start in Map-test
+    $("#createRoot").val(dataTest.entry.createRoot); // createRoot in Map-test
+  }
+}
+
+class ShExMapBaseApp extends ShExBaseApp {
   constructor (base) {
     super(base);
-    Object.assign(this.Caches, {
-      bindings:     new JSONCache($("#bindings1 textarea")),
-      statics:      new JSONCache($("#staticVars textarea")),
-      outputSchema: new SchemaCache($("#outputSchema textarea"), this.shexcParser, this.turtleParser),
-    });
-    Object.assign(this.Getables, {
-      queryStringParm: "bindings",     location: this.Caches.bindings.selection,    cache: this.Caches.bindings    ,
-      queryStringParm: "statics",      location: this.Caches.statics.selection,     cache: this.Caches.statics     ,
-      queryStringParm: "outSchema",    location: this.Caches.outputSchema.selection,cache: this.Caches.outputSchema,
-    });
+    const manifest = new ShExMapManifestCache($("#manifestDrop"));
+    const bindings = new JSONCache($("#bindings1 textarea"));
+    const statics = new JSONCache($("#staticVars textarea"));
+    const outputSchema = new SchemaCache($("#outputSchema textarea"), this.shexcParser, this.turtleParser);
+    Object.assign(this.Caches, { manifest, bindings, statics, outputSchema, });
+    const parameters = [
+      {queryStringParm: "manifest",  location: manifest.selection,    cache: manifest, fail: e => $("#manifestDrop li").text(NO_MANIFEST_LOADED)},
+      {queryStringParm: "bindings",  location: bindings.selection,    cache: bindings    },
+      {queryStringParm: "statics",   location: statics.selection,     cache: statics     },
+      {queryStringParm: "outSchema", location: outputSchema.selection,cache: outputSchema},
+    ];
+    Array.prototype.push.apply(this.Getables, parameters);
+    Array.prototype.push.apply(this.QueryParams, parameters);
   }
 }
 
--- packages/extension-map/doc/ShExMapWorkerApp.js
+++ packages/extension-map/doc/ShExMapWorkerApp.js
@@ -1,3 +1,4 @@
 class ShExMapWorkerApp extends ShExMapBaseApp {
+  usingValidator (_validator) { }
 }
 
