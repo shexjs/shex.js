152cd13b785855517b018bca7fb6bad160f4ba8a
Eric Prud'hommeaux <eric+github@w3.org>
2023-03-11 10:37:25 +0100

    ~ factor out materializeAsync from ShExMapIn{Main,Worker}

--- packages/extension-map/doc/ShExMapInMainApp.js
+++ packages/extension-map/doc/ShExMapInMainApp.js
@@ -1,52 +1,16 @@
-class ShExMapInMainApp extends ShExMapBaseApp {
-  getValidator (loaded, _base, inputData) {
-    const validator = new DirectShExValidator(loaded, inputData, this.makeRenderer());
-    this.Mapper = this.MapModule.register(validator.validator, ShExWebApp);
-    return validator;
+class DirectShExMaterializer {
+  constructor (schema, shapeMap, resultBindings, mapModule, mapper) {
+    // this.trivialMaterializer = this.Mapper.trivialMaterializer(schema);
+    // this.outputGraph = trivialMaterializer.materialize(binder, lexToTerm($("#createRoot").val()), outputShape);
+    this.shapeMap = shapeMap;
+    this.binder = mapper.binder(resultBindings);
+    this.materializer = mapModule.materializer.construct(schema, mapper, {});
   }
-
-  async materializeAsync () {
-    if (this.Caches.bindings.get().trim().length === 0) {
-      this.resultsWidget.replace("You must validate data against a ShExMap schema to populate mappings bindings.").
-        removeClass("passes fails").addClass("error");
-      return null;
-    }
-    this.resultsWidget.start();
-    const parsing = "output schema";
-    try {
-      const outputSchemaText = this.Caches.outputSchema.selection.val();
-      const outputSchemaIsJSON = outputSchemaText.match(/^\s*\{/);
-      const outputSchema = await this.Caches.outputSchema.refresh();
-
-      // const resultBindings = Object.assign(
-      //   await this.Caches.statics.refresh(),
-      //   await this.Caches.bindings.refresh()
-      // );
-
-      function _dup (obj) { return JSON.parse(JSON.stringify(obj)); }
-      let resultBindings = _dup(await this.Caches.bindings.refresh());
-      if (this.Caches.statics.get().trim().length === 0)
-        await this.Caches.statics.set("{  }");
-      const _t = await this.Caches.statics.refresh();
-      if (_t && Object.keys(_t).length > 0) {
-        if (!Array.isArray(resultBindings))
-          resultBindings = [resultBindings];
-        resultBindings.unshift(_t);
-      }
-
-      const outputShapeMap = [this.fixMaterializationShapeMapEntry($("#createRoot").val(), $("#outputShape").val())];
-
-      await this.Caches.bindings.set(JSON.stringify(resultBindings, null, "  "));
-      // const trivialMaterializer = this.Mapper.trivialMaterializer(outputSchema);
-      // const outputGraph = trivialMaterializer.materialize(binder, lexToTerm($("#createRoot").val()), outputShape);
-      const binder = this.Mapper.binder(resultBindings);
+  async invoke (fixedMap, validationTracker, time, _done, _currentAction) {
       const generatedGraph = new RdfJs.Store();
-      $("#results div").empty();
-      $("#results .status").text("materializing data...").show();
-      outputShapeMap.forEach(pair => {
+      this.shapeMap.forEach(pair => {
         try {
-          const materializer = this.MapModule.materializer.construct(outputSchema, this.Mapper, {});
-          const resM = materializer.validate(binder, ShExWebApp.StringToRdfJs.n3idTerm2RdfJs(pair.node), pair.shape);
+          const resM = this.materializer.validate(this.binder, ShExWebApp.StringToRdfJs.n3idTerm2RdfJs(pair.node), pair.shape);
           if ("errors" in resM) {
             this.renderEntry( {
               node: pair.node,
@@ -66,74 +30,23 @@ class ShExMapInMainApp extends ShExMapBaseApp {
           console.dir(e);
         }
       });
-      this.currentRenderer.finish();
-      $("#results .status").text("materialization results").show();
+    time = new Date() - time;
+    $("#shapeMap-tabs").attr("title", "last materialization: " + time + " ms");
+    $("#results .status").text("rendering results...").show();
 
-      // Extract rdf:Collection heads.
-      const lists = generatedGraph.extractLists({
-        remove: true // Remove quads involved in lists (RDF Collections).
-      });
+    return generatedGraph;
+  }
+}
 
-      outputShapeMap.forEach(pair => {
-        const {node, shape} = pair;
-        try {
-          const nestedWriter = new ShExWebApp.NestedTurtleWriter.Writer(null, {
-            // lists: {}, -- lists will require some thinking
-            format: 'text/turtle',
-            // baseIRI: resource.base,
-            prefixes: this.Caches.outputSchema.parsed._prefixes,
-            lists,
-            version: 1.1,
-            indent: '    ',
-            checkCorefs: n => false,
-            // debug: true,
-          });
-          const db = ShExWebApp.RdfJsDb(generatedGraph, null); // no query tracker needed
-          const validator = new ShExWebApp.Validator(outputSchema, db, {
-            results: "api",
-            regexModule: ShExWebApp["eval-simple-1err"],
-          });
-          const res = validator.validateShapeMap([{node, shape}])[0].appinfo;
-          if (!("solution" in res))
-            throw res;
-          const matched = [];
-          const seen = new RdfJs.Store(); // use N3Store to de-duplicate quads that were validated multiple ways.
-          const matchedDb = {
-            addQuad: function (q) {
-              if (!seen.countQuads(q.subject, q.predicate, q.object, q.graph)) {
-                seen.addQuad(q);
-                matched.push(q);
-              }
-            }
-          }
-          ShExWebApp.Util.getProofGraph(res, matchedDb, RdfJs.DataFactory);
-          const rest = new RdfJs.Store();
-          rest.addQuads(generatedGraph.getQuads()); // the resource giveth
-          matched.forEach(q => rest.removeQuad(q)); // the matched taketh away
-          nestedWriter.addQuads(matched.filter(q => ([ShExWebApp.Util.RDF.first, ShExWebApp.Util.RDF.rest]).indexOf(q.predicate.value) === -1));
-          if (rest.size > 0) {
-            nestedWriter.comment("\n# Triples not in the schema:");
-            nestedWriter.addQuads(rest.getQuads())
-          }
-          nestedWriter.end((error, result) => this.addResult(error, result));
-        } catch (e) {
-          console.error(`NestedWriter(${node}@${shape}) failure:`);
-          console.error(e);
-          const fallbackWriter = new RdfJs.Writer({ prefixes: this.Caches.outputSchema.parsed._prefixes });
-          fallbackWriter.addQuads(generatedGraph.getQuads());
-          fallbackWriter.end((error, result) => this.addResult(error, result));
-        }
-      });
-      this.resultsWidget.finish();
-      return { materializationResults: generatedGraph };
-    } catch (e) {
-      if (true) // don't print stack for parser errors
-        console.log(e);
-      this.resultsWidget.replace("error parsing " + parsing + ":\n" + e).
-        removeClass("passes fails").addClass("error");
-      // this.resultsWidget.finish();
-      return null;
-    }
+class ShExMapInMainApp extends ShExMapBaseApp {
+  getValidator (loaded, _base, inputData) {
+    const validator = new DirectShExValidator(loaded, inputData, this.makeRenderer());
+    this.Mapper = this.MapModule.register(validator.validator, ShExWebApp);
+    return validator;
+  }
+
+  getMaterializer (schema, shapeMap, resultBindings) {
+    return new DirectShExMaterializer(schema, shapeMap, resultBindings, this.MapModule, this.Mapper);
   }
 }
 
--- packages/extension-map/doc/ShExMapInWorkerApp.js
+++ packages/extension-map/doc/ShExMapInWorkerApp.js
@@ -1,6 +1,61 @@
+class RemoteShExMaterializer {
+  constructor (schema, shapeMap, resultBindings, resultsWidget, onCancel, outputSchema) {
+    this.generatedGraph = new RdfJs.Store();
+    this.created = new Canceleable(
+      $("#materialize"),
+      onCancel,
+      "materialization aborted, re-start from validation",
+      {
+        request: "materialize",
+        queryMap: shapeMap,
+        outputSchema: schema,
+        resultBindings: resultBindings,
+        options: {track: LOG_PROGRESS},
+      },
+      this.parseUpdatesAndResults.bind(this)
+    ).ready();
+  }
+  async invoke (fixedMap, validationTracker, time, done, currentAction) {
+    const materialized = await this.created;
+    return this.generatedGraph;
+  }
+
+  parseUpdatesAndResults (msg, workerUICleanup, resolve, reject) {
+    switch (msg.data.response) {
+    case "update":
+      this.generatedGraph.addQuads(ShExWebApp.Util.valToN3js(msg.data.results, RdfJs.DataFactory));
+      this.resultGraphs.push(msg.data.results);
+      break;
+
+    case "error":
+      if ("exception" in msg.data) {
+        this.resultsWidget.replace("error materializing:\n" + msg.data.exception).
+          removeClass("passes fails").addClass("error");
+      } else {
+        this.renderEntry({
+          node: msg.data.node,
+          shape: msg.data.shape,
+          status: "errors" in msg.data.results ? "nonconformant" : "conformant",
+          appinfo: msg.data.results,
+          elapsed: -1
+        });
+      }
+      break;
+
+    case "done":
+      workerUICleanup();
+      resolve({ materializionResults: this.resultGraphs });
+    }
+  }
+}
+
 class ShExMapInWorkerApp extends ShExMapBaseApp {
   getValidator (loaded, base, inputData) { // same as ShExInWorkerApp
     return new RemoteShExValidator(loaded, base, inputData, this.makeRenderer(), this.disableResultsAndValidate, "endpoint" in this.Caches.inputData ? this.Caches.inputData.endpoint : null)
+  }
+
+  getMaterializer (schema, shapeMap, resultBindings) {
+    return new RemoteShExMaterializer(schema, shapeMap, resultBindings, this.resultsWidget, this.materialize.bind(this));
   }
 
   makeConsoleTracker () {
@@ -19,153 +74,6 @@ class ShExMapInWorkerApp extends ShExMapBaseApp {
       depth: 0
     };
     return logger;
-  }
-
-  async materializeAsync () {
-    if (this.Caches.bindings.get().trim().length === 0) {
-      this.resultsWidget.replace("You must validate data against a ShExMap schema to populate mappings bindings.").
-        removeClass("passes fails").addClass("error");
-      return null;
-    }
-    this.resultsWidget.start();
-    const parsing = "output schema";
-    try {
-      const outputSchemaText = this.Caches.outputSchema.selection.val();
-      const outputSchemaIsJSON = outputSchemaText.match(/^\s*\{/);
-      const outputSchema = await this.Caches.outputSchema.refresh();
-
-      // const resultBindings = Object.assign(
-      //   await this.Caches.statics.refresh(),
-      //   await this.Caches.bindings.refresh()
-      // );
-
-      function _dup (obj) { return JSON.parse(JSON.stringify(obj)); }
-      let resultBindings = _dup(await this.Caches.bindings.refresh());
-      if (this.Caches.statics.get().trim().length === 0)
-        await this.Caches.statics.set("{  }");
-      const _t = await this.Caches.statics.refresh();
-      if (_t && Object.keys(_t).length > 0) {
-        if (!Array.isArray(resultBindings))
-          resultBindings = [resultBindings];
-        resultBindings.unshift(_t);
-      }
-
-      const outputShapeMap = [this.fixMaterializationShapeMapEntry($("#createRoot").val(), $("#outputShape").val())];
-
-      await this.Caches.bindings.set(JSON.stringify(resultBindings, null, "  "));
-      const generatedGraph = new RdfJs.Store();
-      $("#results div").empty();
-      $("#results .status").text("materializing data...").show();
-
-      const resultGraphs = []
-      const materialized = await new Canceleable(
-        $("#materialize"),
-        materialize,
-        "materialization aborted, re-start from validation",
-        {
-          request: "materialize",
-          queryMap: outputShapeMap,
-          outputSchema: outputSchema,
-          resultBindings: resultBindings,
-          options: {track: LOG_PROGRESS},
-        },
-        parseUpdatesAndResults
-      ).ready();
-
-      function parseUpdatesAndResults (msg, workerUICleanup, resolve, reject) {
-        switch (msg.data.response) {
-        case "update":
-          generatedGraph.addQuads(ShExWebApp.Util.valToN3js(msg.data.results, RdfJs.DataFactory));
-          resultGraphs.push(msg.data.results);
-          break;
-
-        case "error":
-          if ("exception" in msg.data) {
-            this.resultsWidget.replace("error materializing:\n" + msg.data.exception).
-              removeClass("passes fails").addClass("error");
-          } else {
-            this.renderEntry({
-              node: msg.data.node,
-              shape: msg.data.shape,
-              status: "errors" in msg.data.results ? "nonconformant" : "conformant",
-              appinfo: msg.data.results,
-              elapsed: -1
-            });
-          }
-          break;
-
-        case "done":
-          workerUICleanup();
-          resolve({ materializionResults: resultGraphs });
-        }
-      }
-      this.currentRenderer.finish();
-      $("#results .status").text("materialization results").show();
-
-      // Extract rdf:Collection heads.
-      const lists = generatedGraph.extractLists({
-        remove: true // Remove quads involved in lists (RDF Collections).
-      });
-
-      outputShapeMap.forEach(pair => {
-        const {node, shape} = pair;
-        try {
-          const nestedWriter = new ShExWebApp.NestedTurtleWriter.Writer(null, {
-            // lists: {}, -- lists will require some thinking
-            format: 'text/turtle',
-            // baseIRI: resource.base,
-            prefixes: this.Caches.outputSchema.parsed._prefixes,
-            lists,
-            version: 1.1,
-            indent: '    ',
-            checkCorefs: n => false,
-            // debug: true,
-          });
-          const db = ShExWebApp.RdfJsDb(generatedGraph, null); // no query tracker needed
-          const validator = new ShExWebApp.Validator(outputSchema, db, {
-            results: "api",
-            regexModule: ShExWebApp["eval-simple-1err"],
-          });
-          const res = validator.validateShapeMap([{node, shape}])[0].appinfo;
-          if (!("solution" in res))
-            throw res;
-          const matched = [];
-          const seen = new RdfJs.Store(); // use N3Store to de-duplicate quads that were validated multiple ways.
-          const matchedDb = {
-            addQuad: function (q) {
-              if (!seen.countQuads(q.subject, q.predicate, q.object, q.graph)) {
-                seen.addQuad(q);
-                matched.push(q);
-              }
-            }
-          }
-          ShExWebApp.Util.getProofGraph(res, matchedDb, RdfJs.DataFactory);
-          const rest = new RdfJs.Store();
-          rest.addQuads(generatedGraph.getQuads()); // the resource giveth
-          matched.forEach(q => rest.removeQuad(q)); // the matched taketh away
-          nestedWriter.addQuads(matched.filter(q => ([ShExWebApp.Util.RDF.first, ShExWebApp.Util.RDF.rest]).indexOf(q.predicate.value) === -1));
-          if (rest.size > 0) {
-            nestedWriter.comment("\n# Triples not in the schema:");
-            nestedWriter.addQuads(rest.getQuads())
-          }
-          nestedWriter.end((error, result) => this.addResult(error, result));
-        } catch (e) {
-          console.error(`NestedWriter(${node}@${shape}) failure:`);
-          console.error(e);
-          const fallbackWriter = new RdfJs.Writer({ prefixes: this.Caches.outputSchema.parsed._prefixes });
-          fallbackWriter.addQuads(generatedGraph.getQuads());
-          fallbackWriter.end((error, result) => this.addResult(error, result));
-        }
-      });
-      this.resultsWidget.finish();
-      return { materializationResults: generatedGraph };
-    } catch (e) {
-      this.reportMaterializationError(e, "materialization");
-      // this.resultsWidget.replace("error parsing " + parsing + ":\n" + e).
-      //   removeClass("passes fails").addClass("error");
-      // // this.resultsWidget.finish();
-      // return null;
-    }
   }
 }
 
