422f9885c181fae123157a5186858aac0699e88b
Eric Prud'hommeaux <eric+github@w3.org>
2023-06-05 11:08:14 +0200

    ~ add location to shex-loader error message

--- packages/shex-loader/shex-loader.js
+++ packages/shex-loader/shex-loader.js
@@ -312,7 +312,10 @@ const ShExLoaderCjsModule = function (config = {}) {
       loadSchemaImports(schema, importers.concat([url]), resourceLoadControler, schemaOptions)
       return Promise.resolve({mediaType, url, importers, schema})
     } catch (e) {
-      e.message = "error parsing ShEx " + url + ": " + e.message
+      const locStr = e.location
+            ? `(${e.location.first_line}:${e.location.first_column})`
+            : ''
+      e.message = "error parsing ShEx " + url + locStr + ": " + e.message
       return Promise.reject(e)
     }
   }
